2327d3a6ff4f10bb296e2063a1da1e4888bb582b
Shammo06 <mrshammo2018@gmail.com>
2024-03-26 16:00:53 +0600

    modify card

--- src/Component/FoodCard/Card.jsx
+++ src/Component/FoodCard/Card.jsx
@@ -6,60 +6,60 @@ import "swiper/css/pagination";
 
 // import required modules
 import { FreeMode, Autoplay} from "swiper/modules";
+import CardItem from "./CardItem";
 
 
 const Card = () => {
     return (
         <div>
             <Swiper
-                showThumbs={true}
-                spaceBetween={10}
-                freeMode={true}
-                loop={true}
-                autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-                }}
-                
-                modules={[FreeMode, Autoplay]}
-                className="mySwiper  "
-                breakpoints={{
-                    // For small device
-                    640: {
-                    slidesPerView: 1,
-                    },
-                    // For medium device
-                    768: {
-                    slidesPerView: 2,
-                    },
-                    // For larger screens 
-                    1024: {
-                    slidesPerView: 4,
-                    },
-                }}
-            >
+            spaceBetween={10}
+            freeMode={true}
+            loop={true}
+            autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            }}
+            
+            modules={[FreeMode, Autoplay]}
+            className="mySwiper  "
+            breakpoints={{
+                // For small device
+                640: {
+                slidesPerView: 1,
+                },
+                // For medium device
+                768: {
+                slidesPerView: 2,
+                },
+                // For larger screens 
+                1024: {
+                slidesPerView: 4,
+                },
+            }}
+        >
                 <SwiperSlide>
-                <h1>hello</h1>
+                     <CardItem></CardItem>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                <h1>hello2</h1>
+                    <CardItem></CardItem>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                <h2>hello3</h2>
+                    <CardItem></CardItem>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                <h1>hello</h1>
+                    <CardItem></CardItem>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                <h1>hello2</h1>
+                    <CardItem></CardItem>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                <h2>hello3</h2>
+                    <CardItem></CardItem>
                 </SwiperSlide>
 
        
--- src/Component/FoodCard/CardItem.jsx
+++ src/Component/FoodCard/CardItem.jsx
@@ -2,8 +2,13 @@
 
 const CardItem = () => {
     return (
-        <div>
-            
+        <div className='card'>
+            <p></p>
+            <img src="" alt="" />
+            <h3>Pepperoni Pizza</h3>
+            <h6>Di napoles</h6>
+            <h6>$13,99</h6>
+            <button>Add to cart</button>
         </div>
     );
 };
