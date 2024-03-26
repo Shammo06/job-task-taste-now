d38808de8641bfcdbcfd3402b036ce4e033fd80a
Shammo06 <mrshammo2018@gmail.com>
2024-03-27 01:26:07 +0600

    modify banner and responsive

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
                 <h2>Experience food <span>Delivery</span> like no other</h2>
                 <p>We deliver the food of your choice wherever, whenever. Select your food from only the top restaurants in the area, and get it in a flash. Download the app now to discover more.</p>
             </div>
-            <div className="">
-                <img src={banner}  alt="" />
+            <div >
+                <img className="image" src={banner}  alt="" />
             </div>
         </div>
     );
